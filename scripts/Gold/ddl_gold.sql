df62dac5eeb85b5a277a38977ea46386d1f81cb5
selsiyaganesan <selsiyaganesan@gmail.com>
2025-08-10 16:48:22 +0530

    Create ddl_gold.sql

---
+++ scripts/Gold/ddl_gold.sql
@@ -0,0 +1,56 @@
+--===================================================
+--Create dimension table:gold.dim_customer
+--===================================================
+IF OBJECT_ID ('gold.dim_customer','U') IS NOT NULL
+	DROP VIEW gold.dim_customer
+GO
+CREATE VIEW gold.dim_customer AS
+SELECT
+ROW_NUMBER() OVER (ORDER BY cs.cst_id) AS customer_key, --master id
+cs.cst_id AS customer_id,
+cs.cst_key AS customer_number,
+cs.cst_firstname AS firstname,
+cs.cst_lastname AS lastname,
+ec.CNTRY AS country,
+cs.cst_marital_status AS marital_status,
+CASE WHEN cs.cst_gndr!='n/a' THEN cs.cst_gndr
+	 ELSE COALESCE(es.GEN,'n/a')
+END gender,
+es.BDATE AS birthdate,
+cs.cst_create_date AS create_date
+FROM [Silver].[crm_cust_info] AS cs
+LEFT JOIN [Silver].[erp_CUST_AZ12] AS es
+ON cs.cst_key=es.CID
+LEFT JOIN [Silver].[erp_LOC_A101] AS ec
+ON cs.cst_key=ec.CID
+GO
+--===================================================
+--Create dimension table:gold.dim_product
+--===================================================
+IF OBJECT_ID ('gold.dim_product','U') IS NOT NULL
+	DROP VIEW gold.dim_product
+GO
+CREATE VIEW gold.dim_product AS
+SELECT
+ROW_NUMBER() OVER (ORDER BY sp.prd_key) AS product_key,
+sp.prd_id AS product_id,
+sp.prd_key AS product_number,
+sp.prd_nm AS product_name,
+sp.cat_id AS category_id,
+Se.CAT AS category,
+Se.SUBCAT AS sub_category,
+Se.MAINTENANCE AS maintenance,
+sp.prd_cost AS cost,
+sp.prd_line AS product_line,
+sp.prd_start_dt AS start_date
+FROM [Silver].[crm_prd_info] AS sp
+LEFT JOIN [Silver].[erp_PX_CAT_G1V2] AS Se
+ON sp.cat_id=Se.ID
+WHERE prd_end_dt IS NULL --filter out all historical data
+GO
+--===================================================
+--Create dimension table:gold.fact_sales
+--===================================================
+IF OBJECT_ID ('gold.fact_sales','U') IS NOT NULL
+	DROP VIEW gold.fact_sales
+GO
